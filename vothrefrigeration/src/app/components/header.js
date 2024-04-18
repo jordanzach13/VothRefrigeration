8bae60c353dd0efaa2e910419dc747f9699c8dc2
Zach Jordan <a02310139@usu.edu>
2024-04-18 16:13:59 -0600

    header improvement

--- vothrefrigeration/src/app/components/header.js
+++ vothrefrigeration/src/app/components/header.js
@@ -1,18 +1,22 @@
 import Link from "next/link";
 
-export default function Header(){
-  return(
+export default function Header() {
+  return (
     <nav className="bg-secondary text-primary p-4 fixed w-full z-50">
       <div className="flex items-center justify-between mx-4">
         <div className="flex items-center space-x-4">
-          <img className="h-12 w-12" src="/images/logoipsum-245.svg"/>
-          <h1 className="text-2xl font-semibold">Voth Refrigeration & Services</h1>
+          <img className="h-12 w-12" src="/images/logoipsum-245.svg" alt="Logo" />
+          <h1 className="text-xl font-semibold md:text-2xl">Voth Refrigeration & Services</h1>
         </div>
-        <div className="flex items-baseline space-x-2 text-1xl">
-          <Link href="/" className="hover:bg-primary hover:text-secondary rounded-md px-3 py-2">Home</Link>
-          <Link href="/contact" className="hover:bg-primary hover:text-secondary rounded-md px-3 py-2">Contact</Link>
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <Link href="/">
+            <span className="cursor-pointer hover:bg-primary hover:text-secondary rounded-md px-3 py-2 text-sm md:text-base">Home</span>
+          </Link>
+          <Link href="/contact">
+            <span className="cursor-pointer hover:bg-primary hover:text-secondary rounded-md px-3 py-2 text-sm md:text-base">Contact</span>
+          </Link>
         </div>
       </div>
     </nav>
-  )
+  );
 }
