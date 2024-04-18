f4a2594885a55f3802962adb4f79354d4b9373ad
Zach Jordan <a02310139@usu.edu>
2024-04-18 15:37:48 -0600

    rough draft

--- vothrefrigeration/src/app/contact/page.js
+++ vothrefrigeration/src/app/contact/page.js
@@ -1,5 +1,34 @@
-export default function Contact(){
-  return(
-    <main>This is the Contact Page</main>
-  )
+export default function Contact() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-8">
+      <div className="bg-white p-8 rounded-md shadow-md w-full max-w-md">
+        <h1 className="text-3xl font-bold mb-4 text-secondary text-center">Contact Us</h1>
+        
+        <div className="space-y-4 text-secondary">
+          
+          <div className="flex flex-col space-y-2">
+            <h2 className="text-xl font-semibold ">Phone</h2>
+            <p>(435) 512-6158</p>
+          </div>
+          
+          <div className="flex flex-col space-y-2">
+            <h2 className="text-xl font-semibold ">Email</h2>
+            <p>vothservices@gmail.com</p>
+          </div>
+
+          <div className="flex flex-col space-y-2">
+            <h2 className="text-xl font-semibold">Address</h2>
+            <p>P.O. Box 205</p>
+            <p>Wellsive, UT, 84319</p>
+          </div>
+          
+          <div className="flex flex-col space-y-2">
+            <h2 className="text-xl font-semibold ">Hours</h2>
+            <p>Monday - Friday: 7:00 AM - 5:00 PM</p>
+            <p>Saturday & Sunday: Closed</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- vothrefrigeration/src/app/page.js
+++ vothrefrigeration/src/app/page.js
@@ -1,18 +1,34 @@
 import Services from "./components/service";
+import Link from "next/link";
 
 export default function Home() {
-
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-16">
-      <div className="max-w-4xl w-full mt-16 space-y-4">
-        <div className="bg-white rounded-md p-8 shadow-md">
-          <h2 className="text-3xl font-bold mb-4 text-secondary">Mission Statement</h2>
-          <p className="text-lg leading-relaxed text-gray-700">
-            Voth Refrigeration specializes in your commercial and residential needs. Our mission is to make it easy for our clients to do business with us. We take pride in our efficiency, honesty, and quick responses to your needs. We never compromise on the quality of our work and the services we provide.
+    <main className="flex flex-col min-h-screen p-0">
+      <div className="flex flex-grow flex-col w-full mt-16 space-y-8 pb-16">
+
+        {/* Hero Section */}
+        <div className="flex flex-col items-center justify-center text-center h-screen mb-16 bg-hero rounded-md shadow-lg hero-text">
+          <h1 className="text-5xl font-extrabold mb-4">Welcome to Voth Refrigeration</h1>
+          <p className="text-xl leading-relaxed mb-6">
+            Specializing in commercial and residential refrigeration needs.
           </p>
+          <button className="hero-button py-3 px-8 rounded-md shadow-md">
+            <Link href="/contact">Contact Us</Link>
+          </button>
         </div>
-        <div className="text-secondary w-full">
+
+        {/* Mission Statement */}
+        <div className="flex-grow flex justify-center items-center px-8">
+          <div className="bg-white rounded-md p-8 shadow-md max-w-4xl">
+            <h2 className="text-4xl font-bold mb-4 text-secondary text-center">Why Choose Us</h2>
+            <p className="text-lg leading-relaxed text-gray-700 text-center">
+              Voth Refrigeration specializes in your commercial and residential needs. Our mission is to make it easy for our clients to do business with us. We take pride in our efficiency, honesty, and quick responses to your needs. We never compromise on the quality of our work and the services we provide.
+            </p>
+          </div>
+        </div>
+
+        {/* Services Section */}
+        <div className="flex-grow text-secondary px-16">
           <Services/>
         </div>
       </div>
