d78b277bcf8aa7ad0dd4cb0ddb4cc28e04912ea4
Zach Jordan <a02310139@usu.edu>
2024-04-18 23:44:32 -0600

    palette changes

--- vothrefrigeration/src/app/components/header.js
+++ vothrefrigeration/src/app/components/header.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav className="bg-secondary text-primary p-4 fixed w-full z-50">
-      <div className="flex flex-col md:flex-row items-center justify-between mx-4">
+    <nav className="text-primary p-2 fixed w-full z-50 backdrop-filter backdrop-blur-sm nav-border">
+      <div className="flex flex-col md:flex-row items-center justify-between mx-4 backdrop-filter">
         <div className="flex items-center space-x-4">
           <img className="h-12 w-12" src="/images/logoipsum-245.svg" alt="Logo" />
           <h1 className="text-xl font-semibold md:text-2xl">Voth Refrigeration & Services</h1>
         </div>
-        <div className="flex md:flex-row items-center space-x-2 md:space-x-4 mt-4 md:mt-0">
+        <div className="flex flex-col md:flex-row items-center space-x-2 md:space-x-4 mt-4 md:mt-0 ">
           <Link href="/">
-            <span className="cursor-pointer hover:bg-primary hover:text-secondary rounded-md px-3 py-2 text-sm md:text-base">Home</span>
+            <span className="cursor-pointer hover:bg-accent rounded-md px-3 py-2 text-sm border">Home</span>
           </Link>
           <Link href="/contact">
-            <span className="cursor-pointer hover:bg-primary hover:text-secondary rounded-md px-3 py-2 text-sm md:text-base mt-2 md:mt-0 md:ml-2">Contact</span>
+            <span className="bg-secondary text-secondary cursor-pointer hover:bg-accent2 rounded-md px-3 py-2 text-sm border">Contact</span>
           </Link>
         </div>
       </div>
--- vothrefrigeration/src/app/page.js
+++ vothrefrigeration/src/app/page.js
@@ -4,31 +4,37 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-0">
-      <div className="flex flex-grow flex-col w-full mt-16 space-y-8 pb-16">
+      <div className="flex flex-grow flex-col w-full mt-24 space-y-8 pb-16">
 
         {/* Hero Section */}
-        <div className="flex flex-col items-center justify-center text-center h-screen mb-16 bg-hero rounded-md shadow-lg hero-text">
-          <h1 className="text-5xl font-extrabold mb-4">Welcome to Voth Refrigeration</h1>
-          <p className="text-xl leading-relaxed mb-6">
-            Specializing in commercial and residential refrigeration needs.
-          </p>
-          <button className="hero-button py-3 px-8 rounded-md shadow-md">
-            <Link href="/contact">Contact Us</Link>
-          </button>
+        <div className="relative flex flex-col items-center justify-center text-center h-screen mb-16 bg-hero rounded-md shadow-lg hero-text">
+          <div className="relative z-10">
+            <h1 className="text-5xl font-extrabold mb-4">Welcome to Voth Refrigeration</h1>
+            <p className="text-xl leading-relaxed mb-6">
+              Specializing in commercial and residential refrigeration needs.
+            </p>
+            <button className="border hover:bg-accent hero-button py-3 px-8 rounded-md shadow-md">
+              <Link href="/contact">Contact Us</Link>
+            </button>
+          </div>
+          <div className="z-0 flex items-center justify-center absolute pb-48">
+            <div className="triangle-black"></div>
+            <div className="triangle"></div>
+          </div>
         </div>
 
         {/* Mission Statement */}
         <div className="flex-grow flex justify-center items-center px-8">
-          <div className="bg-white rounded-md p-8 shadow-md max-w-4xl">
-            <h2 className="text-4xl font-bold mb-4 text-secondary text-center">Why Choose Us</h2>
-            <p className="text-lg leading-relaxed text-gray-700 text-center">
+          <div className="rounded-md p-8 shadow-md max-w-4xl text-primary">
+            <h2 className="text-4xl font-bold mb-4 text-center">Why Choose Us</h2>
+            <p className="text-lg leading-relaxed text-center">
               Voth Refrigeration specializes in your commercial and residential needs. Our mission is to make it easy for our clients to do business with us. We take pride in our efficiency, honesty, and quick responses to your needs. We never compromise on the quality of our work and the services we provide.
             </p>
           </div>
         </div>
 
         {/* Services Section */}
-        <div className="flex-grow text-secondary px-16">
+        <div className="flex-grow text-primary px-16">
           <Services/>
         </div>
       </div>
